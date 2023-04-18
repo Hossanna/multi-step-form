788dfe4ced19fb790fe2864d9ae026d4b917b888
Hosanna Bivan <hosabivan@gmail.com>
2023-04-18 10:45:21 +0100

    select plan

--- js/script.js
+++ js/script.js
@@ -3,10 +3,10 @@ const toggleSpan = document.getElementById("toggle")
 const month = document.getElementById('month')
 const year = document.getElementById('year')
 const boxes = document.querySelectorAll(".flexxx")
-const span = document.querySelectorAll(".span-yr")
-var nine = document.getElementById("nine")
-var twelve = document.getElementById("twelve")
-var fifteen = document.getElementById("fifteen")
+const hide = document.querySelectorAll(".hide")
+const show = document.querySelectorAll(".show")
+const monthsFree = document.querySelectorAll(".monthsFree")
+
 
 
 const toggleButton = document.createElement('toggleButton'); 
@@ -16,6 +16,7 @@ toggleSpan.appendChild(toggleButton);
 const toggleSwitchCircle = document.createElement('toggleSwitchCircle'); 
 toggleButton.appendChild(toggleSwitchCircle); 
 
+
 function toggleSwitchTransformFunction() { 
   if(defaultSetting == "off"){ 
 
@@ -23,37 +24,28 @@ function toggleSwitchTransformFunction() {
     toggleSwitchCircle.style.transform = "translateX(100%)" 
     year.style.color = "hsl(213, 96%, 18%)"
     month.style.color = "hsl(231, 11%, 63%)"
-    // toggleButton.style.background = "black"
 
 
     // execute code when ON
     for (i=0; i<=boxes.length; i++){
-      let monthsFree = document.createElement("span")
-      monthsFree.classList.add("span-yr")
-      monthsFree.innerHTML = "2 months free"
-      boxes[i].appendChild(monthsFree)
+      show[i].style.display = "none"
+      hide[i].style.display = "block"
+      monthsFree[i].style.display ="block"
     }
-    nine.innerHTML = "$90/yr"
-    twelve.innerHTML = "$120/yr"
-    fifteen.innerHTML = "$150/yr"
-
+    
 
   } else if(defaultSetting == "on"){ 
     defaultSetting = "off" 
     toggleSwitchCircle.style.transform = "translateX(0%)"
     month.style.color = "hsl(213, 96%, 18%)"
     year.style.color = "hsl(231, 11%, 63%)" 
-    // toggleButton.style.background = "white"
 
     // execute code when OFF
     for (i=0; i<=boxes.length; i++){
+      show[i].style.display = "block"
+      hide[i].style.display = "none"
+      monthsFree[i].style.display ="none"
 
-      
-      // boxes[i].removeChild(monthsFree)
-      // boxes[i].removeChild(span)
-      // span.remove()
-
-      // let monthsFree = document.remove("span")
     }
 
 
